6770a50bd75c9b9f5b3be8c93e1aecd53d62d906
Sriram Krishna <ksri91@gmail.com>
2022-12-09 17:09:41 +0530

    ✨ Day 6 Part 1

---
+++ day06/part1.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    for i in range(len(s) - 3):
+        if len(set(s[i : i + 4])) == 4:  # noqa: E203
+            return i + 4
+    return 0
+
+
+INPUT_S = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+EXPECTED = 7
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        (INPUT_S, EXPECTED),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():  # type: ignore[attr-defined]
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
