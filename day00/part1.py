c1636f93bba2f6cdf41bfc0c3fa93eb4de503e0b
Sriram Krishna <ksri91@gmail.com>
2022-12-09 16:38:19 +0530

    👷 flake8 config + a mypy ignore

--- day00/part1.py
+++ day00/part1.py
@@ -7,14 +7,10 @@ import pytest
 
 import support
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
 
 
 def compute(s: str) -> int:
-    numbers = support.parse_numbers_split(s)
-    for n in numbers:
-        pass
-
     lines = s.splitlines()
     for line in lines:
         pass
@@ -22,16 +18,14 @@ def compute(s: str) -> int:
     return 0
 
 
-INPUT_S = '''\
-'''
+INPUT_S = """\
+"""
 EXPECTED = 1
 
 
 @pytest.mark.parametrize(
-    ('input_s', 'expected'),
-    (
-        (INPUT_S, EXPECTED),
-    ),
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
 )
 def test(input_s: str, expected: int) -> None:
     assert compute(input_s) == expected
@@ -39,14 +33,14 @@ def test(input_s: str, expected: int) -> None:
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
     args = parser.parse_args()
 
-    with open(args.data_file) as f, support.timing():
+    with open(args.data_file) as f, support.timing():  # type: ignore[attr-defined]
         print(compute(f.read()))
 
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
