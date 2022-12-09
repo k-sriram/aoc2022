45ac6168e4f26e9b6f522573ddece2f2c4d65405
Sriram Krishna <ksri91@gmail.com>
2022-12-09 16:50:50 +0530

    ✨ Day 3 Part 1

---
+++ day03/part1.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    priority = 0
+
+    lines = s.splitlines()
+    for line in lines:
+        n = len(line)
+        priority += get_priority(
+            set(line[: n // 2]).intersection(line[n // 2 :]).pop()  # noqa: E203
+        )
+    return priority
+
+
+def get_priority(c: str) -> int:
+    spam = (ord(c) - 96) % 64
+    if spam > 30:
+        spam -= 6
+    return spam
+
+
+INPUT_S = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
+EXPECTED = 157
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():  # type: ignore[attr-defined]
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
