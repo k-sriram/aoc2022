6f5bee1031ad4f0143421eb1fcfd471b554895d4
Sriram Krishna <ksri91@gmail.com>
2022-12-18 12:52:04 +0530

    ✨ Day 18 Part 1

---
+++ day18/part1.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    cubes = [eval(line) for line in s.splitlines()]
+    area = 6 * len(cubes)
+    for i, c1 in enumerate(cubes):
+        for c2 in cubes[i + 1 :]:
+            if sum(abs(a - b) for a, b in zip(c1, c2)) == 1:
+                area -= 2
+    return area
+
+
+INPUT_S = """\
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
+"""
+EXPECTED = 64
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():  # type: ignore[attr-defined]
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
